b561d58c4236b8f2265626d6c36eb2f89c1bd495
Maxonchicks <120844547+Maxonchicks@users.noreply.github.com>
2023-12-09 10:48:58 +0000

    final

--- lab7/src/udpserver.c
+++ lab7/src/udpserver.c
@@ -7,15 +7,76 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <stdbool.h>
 
-#define SERV_PORT 20001
-#define BUFSIZE 1024
+// #define SERV_PORT 20001
+// #define BUFSIZE 1024
 #define SADDR struct sockaddr
 #define SLEN sizeof(struct sockaddr_in)
 
-int main() {
+int main(int argc, char **argv) {
+  int port = -1;
+  int bufsize = -1;
+  while (true)
+  {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"bufsize", required_argument, 0, 0},
+                                      {"port", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c)
+    {
+    case 0:
+    {
+      switch (option_index)
+      {
+      case 0:
+        bufsize = atoi(optarg);
+        if (bufsize < 0)
+        {
+          printf("bufsize should be a positive integer!\n");
+          return -1;
+        }
+        break;
+      case 1:
+        port = atoi(optarg);
+        if (port < 1 || port > 65535)
+        {
+          printf("port should be an integer between 1 and 65535!\n");
+          return -1;
+        }
+        break;
+      default:
+        printf("Index %d is out of options\n", option_index);
+      }
+    }
+    break;
+
+    case '?':
+      printf("Unknown argument\n");
+      break;
+    default:
+      fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (bufsize == -1 || port == -1)
+  {
+    fprintf(stderr, "Using: %s --bufsize 100 --port 5050\n",
+            argv[0]);
+    return 1;
+  }
+
   int sockfd, n;
-  char mesg[BUFSIZE], ipadr[16];
+  char mesg[bufsize], ipadr[16];
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
@@ -27,7 +88,7 @@ int main() {
   memset(&servaddr, 0, SLEN);
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
+  servaddr.sin_port = htons(port);
 
   if (bind(sockfd, (SADDR *)&servaddr, SLEN) < 0) {
     perror("bind problem");
@@ -38,7 +99,7 @@ int main() {
   while (1) {
     unsigned int len = SLEN;
 
-    if ((n = recvfrom(sockfd, mesg, BUFSIZE, 0, (SADDR *)&cliaddr, &len)) < 0) {
+    if ((n = recvfrom(sockfd, mesg, bufsize, 0, (SADDR *)&cliaddr, &len)) < 0) {
       perror("recvfrom");
       exit(1);
     }
